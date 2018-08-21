4acff3272619b7a2b8920e017890dc1f055e1c73
Chanderveersen <42332612+Chanderveersen@users.noreply.github.com>
2018-08-21 17:39:02 +0530

    Add files via upload

---
+++ assignment_2.java
@@ -0,0 +1,10 @@
+ class assignment_2{
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
